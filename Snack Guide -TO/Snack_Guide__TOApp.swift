165df9aed2d65c3025ba76871d268bdc3d21b879
Will Ngo <s3938145@rmit.edu.vn>
2023-08-05 12:56:02 +0700

    Initial Commit

---
+++ Snack Guide -TO/Snack_Guide__TOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snack_Guide__TOApp.swift
+//  Snack Guide -TO
+//
+//  Created by Will Ngo on 2023-08-05.
+//
+
+import SwiftUI
+
+@main
+struct Snack_Guide__TOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
